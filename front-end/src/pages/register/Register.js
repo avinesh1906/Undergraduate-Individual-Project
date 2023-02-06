157c33fede9d940e6b161b25a3c41e28a0ac3df5
Avinesh Culloo <avineshculloo@gmail.com>
2023-02-06 23:10:10 +0400

    feat: registration of user

--- front-end/src/pages/register/Register.js
+++ front-end/src/pages/register/Register.js
@@ -1,8 +1,14 @@
+import { ethers } from 'ethers';
+// import { ethers } from "ethers";
 import React, { useState } from 'react';
 // import Web3 from 'web3';
-// import Contract from 'truffle-contract';
-import { ethers } from 'ethers';
-import { parseEther, formatEther } from '@ethersproject/units';
+import Individual from '../../contracts/individual.json';
+// import { ethers } from 'ethers';
+// import { parseEther, formatEther } from '@ethersproject/units';
+
+const IndividualContractAddress = '0x303436626f2EC900060006EEE8f59fbd74313DDf';
+let address, signer, provider;
+
 
 const Register = () => {
   const [firstName, setFirstName] = useState('');
@@ -10,11 +16,37 @@ const Register = () => {
   const [email, setEmail] = useState('');
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
- 
+
+  const [isConnected, toggleConnected] = useState(0);
+
+  function setAddress(ethaddy) {
+    address = ethaddy;
+    if (address != null) {  toggleConnected ( !isConnected ); }
+    console.log("Account:", address);
+    alert("Connected: " + address);
+  }
+
+  async function connectWallet() {
+    provider = new ethers.providers.Web3Provider(window.ethereum);
+    // Prompt user for account connections
+    await provider.send("eth_requestAccounts", []);
+    signer = provider.getSigner();
+    setAddress( await signer.getAddress() );
+    let balance = await signer.getBalance();
+    console.log(await ethers.utils.formatEther(balance));
+  }
+  
+  async function registerIndividual() {
+    const individualContract = new ethers.Contract(IndividualContractAddress, Individual.abi, signer);
+    const reponse = await individualContract.registerProvider("avi", "culloo", "Avi1906", "avi@gmail.com", "123");
+    console.log(reponse);
+  }
 
   const handleSubmit = (event) => {
     event.preventDefault();
     // Add logic to handle form submission
+    if (!isConnected) {connectWallet()}
+      else {registerIndividual()}
   }
   return (
     <form onSubmit={handleSubmit}>
@@ -63,7 +95,7 @@ const Register = () => {
         />
       </label>
       <br />
-      <button type="submit">Submit</button>
+      <button type="submit">{(isConnected) ? 'Submit Form' : 'CONNECT WALLET'}</button>
     </form>
   );
 };
