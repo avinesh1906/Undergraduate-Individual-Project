77bb86f6b150cbf9530446746dabbc918451cff4
Avinesh Culloo <avineshculloo@gmail.com>
2023-02-10 19:14:20 +0400

    feat: create the health organization sol file

--- back-end/contracts/HealthOrganization.sol
+++ back-end/contracts/HealthOrganization.sol
@@ -2,17 +2,15 @@
 
 pragma solidity ^0.8.0;
 
-contract healthorganization {
+contract HealthOrganization {
     // fields
     address healthorganizationAddress;
-    string healthorganizationName = "Health and Quality of Life Organization";
-    string email = "hio@gmail.com";
-    bytes32 password = hashPassword("password");
+    string name;
+    string email;
+    bytes32 password;
 
-     constructor() {
-        healthorganizationAddress = msg.sender; 
-    }
-
+    // events
+    event LogRegisterdHO(string name);
 
     // functions
     function hashPassword(string memory _password) private pure returns (bytes32) {
@@ -21,9 +19,21 @@ contract healthorganization {
 
     function getHealthOrganization() public view returns (string memory, string memory){
         return (
-            healthorganizationName,
+            name,
             email
         );
     }
 
+    function registerHealthOrganization(string memory _name, string memory _email, string memory _password) public {
+        healthorganizationAddress = msg.sender;
+        name = _name;
+        email = _email;
+        password = hashPassword(_password);
+
+        emit LogRegisterdHO(name);
+    }
+
+    function isHealthOrganizationRegistered() public view returns (bool) {
+        return healthorganizationAddress != address(0);
+    }
 }
