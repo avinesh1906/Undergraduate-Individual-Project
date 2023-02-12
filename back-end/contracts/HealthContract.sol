2448267f15a6fdf57e36f883be8979d3982290b7
Avinesh Culloo <avineshculloo@gmail.com>
2023-02-12 14:36:29 +0400

    feat: initial health contract

--- back-end/contracts/HealthContract.sol
+++ back-end/contracts/HealthContract.sol
@@ -5,12 +5,17 @@ contract HealthContract {
     // Fields
     address insuranceCompany;
     string coverageType;
+    mapping (address => bool) individuals;
     
     // Enumeration for coverage types
     enum Coverage { Bronze, Silver, Gold, Premium }
 
     // Events
     event LogHealthCoverage(address company, string coverage);
+    event NewClaimSubmitted(address claimant);
+    event NewClaimRequested(address requester);
+    event ClaimApproved(address requester);
+    event ClaimRejected(address requester);
 
     // Functions
     function getCoverageDetails() public view returns (address, string memory) {
@@ -32,5 +37,29 @@ contract HealthContract {
         if (_coverageType == Coverage.Silver) return "Silver";
         if (_coverageType == Coverage.Gold) return "Gold";
         if (_coverageType == Coverage.Premium) return "Premium";
+        return "Invalid Coverage Type";
+    }
+
+    function submitClaim(address _individual) public {
+        require(individuals[_individual], "Individual not registered in the contract");
+        individuals[_individual] = false;
+        emit NewClaimSubmitted(_individual);
+    }
+
+    function requestClaim(address _individual) public {
+        require(!individuals[_individual], "Claim already submitted by this individual");
+        emit NewClaimRequested(_individual);
+    }
+
+    function approveClaim(address _individual) public {
+        require(!individuals[_individual], "Claim already approved");
+        individuals[_individual] = true;
+        emit ClaimApproved(_individual);
+    }
+
+    function rejectClaim(address _individual) public {
+        require(!individuals[_individual], "Claim already rejected");
+        individuals[_individual] = true;
+        emit ClaimRejected(_individual);
     }
 }
