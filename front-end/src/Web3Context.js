04ee6055a8522884342f3a43c0120f73f15d29b2
Avinesh Culloo <avineshculloo@gmail.com>
2023-02-21 11:43:47 +0400

    feat: create the Web3Context.js file

---
+++ front-end/src/Web3Context.js
@@ -0,0 +1,34 @@
+import React, { createContext, useState } from 'react';
+import { ethers } from 'ethers';
+
+// Create a new context to store the provider, signer, and address variables
+export const Web3Context = createContext();
+
+
+// Create a provider component that will wrap the App component
+export const Web3Provider = ({ children }) => {
+  const [provider, setProvider] = useState();
+  const [signer, setSigner] = useState();
+  const [address, setAddress] = useState();
+
+	// Connect to the Web3 provider and update the context state
+	const connectWeb3 = async () => {
+		// provider = new ethers.providers.Web3Provider(window.ethereum);
+    // Prompt user for account connections
+    // await provider.send("eth_requestAccounts", []);
+		const web3Provider = new ethers.providers.JsonRpcProvider('http://127.0.0.1:7545');
+    const web3Signer = web3Provider.getSigner();
+		const web3Address = await web3Signer.getAddress();
+
+		setProvider(web3Provider);
+		setSigner(web3Signer);
+		setAddress(web3Address);
+	};
+
+	return (
+    <Web3Context.Provider value={{ provider, signer, address, connectWeb3 }}>
+      {children}
+    </Web3Context.Provider>
+  );
+	
+};
