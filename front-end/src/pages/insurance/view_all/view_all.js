ecdc5487d1d305ff99330c2cc0d17d8c3465a904
Avinesh Culloo <avineshculloo@gmail.com>
2023-03-23 12:01:05 +0400

    feat: create the claim lis

--- front-end/src/pages/insurance/view_all/view_all.js
+++ front-end/src/pages/insurance/view_all/view_all.js
@@ -5,6 +5,7 @@ import ClaimContract from '../../../contracts/ClaimContract.json';
 import contractAddresses from "../../../config";
 import Loader from "../../../components/loader/loader";
 import notFound from '../../../images/view_contract/notFound.jpg'
+import './styles.css';
 
 const ClaimContractAddress = contractAddresses.ClaimContract;
 
@@ -28,15 +29,57 @@ const ViewAllClaims = () => {
     <>
         <div style={{ display: isLoading ? "none" : "block" }}> 
             {claims.length > 0 ? (
-                <div>
-                    {claims.length > 0 &&
-                        claims.map((claim, index) => (
-                                <div key={index}>
-                                    <p>Claim Status: {claim.status}</p>
-                                </div>
-                            )
-                        )
-                    }
+                <div className="u-body u-xl-mode" data-lang="en">
+                    <section className="u-clearfix u-palette-5-light-2 u-section-6" id="sec-9b24">
+                        <div className="u-clearfix u-sheet u-sheet-1">
+                            <h1 className="u-align-center u-text u-text-default u-text-1" spellCheck="false">List of Claims<br/></h1>
+                            <div className="u-expanded-width u-table u-table-responsive u-table-1">
+                                <table className="u-table-entity">
+                                    <colgroup>
+                                    <col width="11.6%"/>
+                                    <col width="22.5%"/>
+                                    <col width="17%"/>
+                                    <col width="17%"/>
+                                    <col width="15%"/>
+                                    <col width="16.90000000000001%"/>
+                                    </colgroup>
+                                    <thead className="u-align-center u-table-header u-table-header-1">
+                                    <tr style={{ height: '29px' }}>
+                                        <th className="u-table-cell"></th>
+                                        <th className="u-table-cell" spellCheck="false">Claimaint</th>
+                                        <th className="u-table-cell">Requester</th>
+                                        <th className="u-table-cell">Health Contract<br/></th>
+                                        <th className="u-table-cell">Status</th>
+                                        <th className="u-table-cell">Claim Amount<br/></th>
+                                    </tr>
+                                    </thead>
+                                    <tbody className="u-align-center u-table-body">
+                                        {claims.length > 0 &&
+                                            claims.map((claim, index) => (
+                                                <tr key={index}>
+                                                    <td className="u-black u-first-column u-table-cell u-table-cell-7">1</td>
+                                                    <td className="u-table-cell">Josh Anderson,<br/>josh-info@gmail.com</td>
+                                                    <td className="u-table-cell">avineshculloo@gmail.com</td>
+                                                    <td className="u-table-cell">Silver</td>
+                                                    <td className="u-table-cell">Submitted</td>
+                                                    <td className="u-table-cell">Rs 10, 000<br/></td>
+                                                </tr>
+                                                )
+                                            )
+                                        }
+                                        <tr>
+                                            <td className="u-black u-first-column u-table-cell u-table-cell-13">2</td>
+                                            <td className="u-palette-5-light-1 u-table-cell u-table-cell-14">Lina Hudson,<br/>lina-h@gmail.com</td>
+                                            <td className="u-palette-5-light-1 u-table-cell u-table-cell-15">keshavculloo@gmail.com</td>
+                                            <td className="u-palette-5-light-1 u-table-cell u-table-cell-16">Bronze</td>
+                                            <td className="u-palette-5-light-1 u-table-cell u-table-cell-17">Approved</td>
+                                            <td className="u-palette-5-light-1 u-table-cell u-table-cell-18">Rs 36, 000<br/></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </section>
                 </div>
             ):(
                 <div className="u-body u-xl-mode" data-lang="en">
