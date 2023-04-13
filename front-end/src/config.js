80f0a4516bff328cdf2895ce4cba0d8efd482d3f
Avinesh Culloo <avineshculloo@gmail.com>
2023-04-13 12:39:07 +0400

    feat: config.js

---
+++ front-end/src/config.js
@@ -0,0 +1,8 @@
+const contractAddresses = {
+    ClaimContract: '0x481aA1AC302888d13278d8d4dBC430942638C213',
+    HealthOrganization: '0x1b6140F68687760f6fC7594358BB835364ad7f20',
+    Individual: '0xD9105C6F29685226b45d3B9BDf0d59D26CdC3311',
+    InsuranceProvider: '0xcbe512D32DfeCe25406d1bF2D6E721213e6836E7'
+};
+
+export default contractAddresses;
