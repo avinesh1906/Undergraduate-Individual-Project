caf9cce6b967a2bd94e7065637c4e639e70db7ff
AlyanaNidhiMDX <101712394+AlyanaNidhiMDX@users.noreply.github.com>
2024-05-14 11:35:34 +0400

    Delete front-end directory

--- front-end/src/config.js
+++
@@ -1,8 +0,0 @@
-const contractAddresses = {
-    ClaimContract: '0x9616d62258E43E9b15005797AFe0756E05B16Db7',
-    HealthOrganization: '0x5c0Ab2Ff4587829FD16d1a68f02c387E1e49F1ED',
-    Individual: '0x099Cb2a3694c6826569f5A948886a22551797dE3',
-    InsuranceProvider: '0x2253E2726403DEa7Bc08b3BA00612b329bB2f64c'
-};
-
-export default contractAddresses;
